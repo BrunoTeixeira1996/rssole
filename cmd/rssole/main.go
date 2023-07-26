ed03d551d71b4836884b6474d50e9ce8c54df173
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2023-07-26 18:22:36 +0100

    gokrazy: adding support for gokrazy

--- cmd/rssole/main.go
+++ cmd/rssole/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/TheMightyGit/rssole/internal/rssole"
+	"github.com/u-root/uio/cp"
 )
 
 const (
@@ -40,11 +41,44 @@ func getFeedsFileConfigSection(filename string) configSection {
 	return c.Config
 }
 
+func runningInGoKrazy() (string, error) {
+	currentPath, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	// copy required files to /perm/home/rssole/
+	errFeeds := cp.Copy("/etc/rssole/feeds.json", "/perm/home/rssole/feeds.json")
+	errReadCache := cp.Copy("/etc/rssole/readcache.json", "/perm/home/rssole/readcache.json")
+	if errFeeds != nil || errReadCache != nil {
+		return "", err
+	}
+
+	return currentPath, nil
+}
+
 func main() {
-	var configFilename, configReadCacheFilename string
+	var configFilename, configReadCacheFilename, currentGoKrazyPath string
+	var err error
+
 	flag.StringVar(&configFilename, "c", "feeds.json", "config filename")
 	flag.StringVar(&configReadCacheFilename, "r", "readcache.json", "readcache location")
+	gokrazyFlag := flag.Bool("gokrazy", false, "use this if you are using gokrazy")
+
 	flag.Parse()
+
+	if *gokrazyFlag {
+		currentGoKrazyPath, err = runningInGoKrazy()
+
+		// If there is an error we should exit
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// now we have the correct locations of these two files in /perm/home/rssole
+		configFilename = currentGoKrazyPath + "/" + configFilename
+		configReadCacheFilename = currentGoKrazyPath + "/" + configReadCacheFilename
+	}
 
 	cfg := getFeedsFileConfigSection(configFilename)
 
